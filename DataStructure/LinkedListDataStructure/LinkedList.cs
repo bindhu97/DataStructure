8924535af015f751edebae955d6f6fcb3dfb2691
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-15 17:06:54 +0530

    [Bindhu]Created Simple Linked List

---
+++ DataStructure/LinkedListDataStructure/LinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.LinkedListDataStructure
+{
+    internal class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Linked List", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ DataStructure/LinkedListDataStructure/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace DataStructure.LinkedListDataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Operations");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
